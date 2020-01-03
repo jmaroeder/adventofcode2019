b5e363e1f60ea8fd9e3c1d0dbaf875438e281ab4
James Roeder <jmaroeder@gmail.com>
2020-01-03 14:05:25 -0500

    4-b

--- adventofcode2019/day4.py
+++ adventofcode2019/day4.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import re
+from contextlib import suppress
 from typing import Union
 
 LOG = logging.getLogger(__name__)
@@ -19,10 +20,21 @@ def password_is_valid(password: Union[int, str]) -> bool:
         if cur == prev:
             found_double = True
         prev = cur
-    if found_double:
-        LOG.debug(password)
+    # if found_double:
+    #     LOG.info(password)
     return found_double
 
+
+def password_is_valider(password: Union[int, str]) -> bool:
+    password = str(password)
+    if not password_is_valid(password):
+        return False
+
+    for digit in password:
+        if password.count(digit) == 2:
+            return True
+
+    return False
 
 def parta(domain: str) -> int:
     lowest, highest = re.match(r'(\d+)-(\d+)', domain).groups()
@@ -32,22 +44,21 @@ def parta(domain: str) -> int:
     return sum(password_is_valid(password) for password in range(lowest, highest + 1))
 
 
-# def parta(wire_paths: Iterable[str]) -> int:
-#     panel = FrontPanel(*wire_paths)
-#     return manhattan_distance(panel.closest_manhattan)
-#
-#
-# def partb(wire_paths: Iterable[str]) -> int:
-#     panel = FrontPanel(*wire_paths)
-#     return panel.closest_steps
+def partb(domain: str) -> int:
+    lowest, highest = re.match(r'(\d+)-(\d+)', domain).groups()
+    lowest = int(lowest)
+    highest = int(highest)
+
+    return sum(password_is_valider(password) for password in range(lowest, highest + 1))
+
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=argparse.FileType('r'), nargs='?',
                         default='../inputs/day4.txt')
     args = parser.parse_args()
     input_data = args.infile.read()
     print(parta(input_data))
-    # print(partb(input_data.splitlines()))
+    print(partb(input_data))
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from adventofcode2019.day4 import password_is_valid
+from adventofcode2019.day4 import password_is_valid, password_is_valider
 
 
 @pytest.mark.parametrize(
@@ -17,13 +17,16 @@ def test_password_is_valid(password, is_valid, caplog):
     assert password_is_valid(password) == is_valid
 
 
-# @pytest.mark.parametrize(
-#     ('wire_paths', 'dist'), [
-#         (('R8,U5,L5,D3', 'U7,R6,D4,L4'), 30),
-#         (('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 610),
-#         (('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 410),
-#     ],
-# )
-# def test_partb(wire_paths, dist, caplog):
-#     caplog.set_level(logging.DEBUG)
-#     assert partb(wire_paths) == dist
+@pytest.mark.parametrize(
+    ('password', 'is_valid'), [
+        (111111, False),
+        (223450, False),
+        (123789, False),
+        (112233, True),
+        (123444, False),
+        (111122, True),
+    ],
+)
+def test_password_is_valider(password, is_valid, caplog):
+    caplog.set_level(logging.DEBUG)
+    assert password_is_valider(password) == is_valid
